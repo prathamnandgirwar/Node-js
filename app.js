508693bccf8c9f7f3e7d0651a8d7a5a01707469a
Pramod Dilipakumar <109644106+pramoddilipakumar@users.noreply.github.com>
2023-07-29 14:28:32 +0530

    Created app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, this is your DevOps project!');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
